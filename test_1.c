fffdd4cf7deeebf3af626d2efda78eca57bae5a2
seunghyunlee <seunghyun.lee@yonsei.ac.kr>
2024-09-25 19:26:50 +0900

    git push origin master

--- test_1.c
+++ test_1.c
@@ -6,7 +6,6 @@ int
 main(int argc, char *argv[]) {
     char buf[100];
 	int base_read = getreadcount();
-    int base_write = getwritecount();
 	
     for (int i = 0; i < 1000; i++) (void) read(4, buf, 1);
 
@@ -15,36 +14,24 @@ main(int argc, char *argv[]) {
 
     if (rc > 0) {
 	    (void) wait();
-		for (int i = 0; i < 1000; i++) {
-	    	(void) read(4, buf, 1);
-        	if(i % 3 == 0) (void) write(4, buf, 1);
-    	}
-
+		for (int i = 0; i < 1000; i++) (void) read(4, buf, 1);
+    	
 		int parent_read = getreadcount();
-    	int parent_write = getwritecount();
 
     	printf(1, "Parent Read: %d\n", parent_read - base_read);
-    	printf(1, "Parent Write: %d\n", parent_write - base_write);
 
 		if(parent_read - base_read == 2000)	right = 1;
-    	if(parent_write - base_write == 334) right *= 1;
 		
     }
     else {
 		char buf[100];
-		for (int i = 0; i < 500; i++) {
-	    	(void) read(4, buf, 1);
-      		(void) write(4, buf, 1);
-		}
-
+		for (int i = 0; i < 500; i++) (void) read(4, buf, 1);
+		
 		int child_read = getreadcount();
-  		int child_write = getwritecount();
 
         printf(1, "Child Read: %d\n", child_read - base_read);
-        printf(1, "Child Write: %d\n", child_write - base_write);
 
 		if(child_read - base_read == 1500) right = 1;
- 		if(child_write - base_write == 500)	right *= 1;
     }
     
     if(right) printf(1, "You are right!\n");
